da53aa7d8922438aea0755b87e7ed9fe467882a0
Yu-TingWang <48304716+Yu-TingWang@users.noreply.github.com>
2019-03-20 14:45:27 -0400

    Add files via upload

--- display/tank.v
+++ display/tank.v
@@ -14,10 +14,13 @@ module tank
         VGA_SYNC_N,                        //    VGA SYNC
         VGA_R,                           //    VGA Red[9:0]
         VGA_G,                             //    VGA Green[9:0]
-        VGA_B,                           //    VGA Blue[9:0]
+        VGA_B,
+		  SW
+
+		  //    VGA Blue[9:0]
     );
     input   CLOCK_50;                //    50 MHz
-    input   [9:0]   SW;
+    input   [17:0]   SW;
     input   [3:0]   KEY;
     // Declare your inputs and outputs here
     // Do not change the following outputs
@@ -31,7 +34,9 @@ module tank
     output    [9:0]    VGA_B;                   //    VGA Blue[9:0]
    
     wire resetn;
+	 wire wipe;
     assign resetn = 1'b1 ;
+	 assign SW17 = wipe;
    
     // Create an Instance of a VGA controller - there can be only one!
     // Define the number of colours as well as the initial background
@@ -52,10 +57,23 @@ module tank
             .VGA_BLANK(VGA_BLANK_N),
             .VGA_SYNC(VGA_SYNC_N),
             .VGA_CLK(VGA_CLK));
-    defparam VGA.RESOLUTION = "160x120";
+	 // cannot be changed due to the restriction of our vga adapter
+    defparam VGA.RESOLUTION = "160x120"; 
     defparam VGA.MONOCHROME = "FALSE";
     defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
     defparam VGA.BACKGROUND_IMAGE = "black.mif";
+	 
+	 // instantiate to erase the backgroun
+	 wire [6:0] x_out, y_out;
+	 wire [2:0] col_out;
+	 reset_background reset(.clock(CLOCK_50), .drawEn(wipe), .x_out(x_out), .y_out(y_out), .col_out(col_out));
+	 // tank's x cooridinate
+	 assign x = wipe ? x_out: w_x;
+	 // tank's y coordinate
+	 assign y = wipe ? y_out: w_y;
+	 // if wipe is toggled, fill the monitor with black
+	 // otherwise, tank color
+	 assign color = wipe ? col_out: output_colour;
 	 
 	 //-----------------------
 	
@@ -63,22 +81,60 @@ module tank
     // for the VGA controller, in addition to any other functionality your design may require.
 	reg tank = 26'b00000011111111111111111111;
     wire ld_x, ld_y;
-    wire [2:0] colour;
-    wire [2:0] white = 3'b111;
-    wire [6:0] x;
-    r [6:0] y;
-    reg  writeEn;
-    wire [3:0] stateNum;
-    reg [6:0] init_x = 7'b1111111;
-    reg [6:0] init_y = 7'b1111111;
+	 wire write;
+	 wire [6:0] w_x, w_y;
     wire [2:0] color;
+    reg [2:0] white = 3'b111;
+    wire [6:0] x;
+    wire [6:0] y;
+    wire  writeEn;
+    wire [3:0] stateNum;
+	 // take 6-bits input for the initial position for player1 and player2
+    reg [5:0] init_x = 6'b111111;
+    reg [5:0] init_y = 6'b000000;
+	 // take the output to be the color
+    wire [2:0] output_colour;
+	 // counter input shu yao hua ji ge
     reg [25:0] ccc = 26'b0;
     // Instansiate datapath 
-    datapath d(.clk(CLOCK_50), .ld_x(ld_x), .ld_y(ld_y), .in_x(init_x), .in_y(init_y), .reset_n(resetn), .x(signal_x), .y(signal_y), .colour(color), .write(writeEn), .stateNum(stateNum), .init_y(init_y), .color(white));
+    datapath d(.clk(CLOCK_50), .ld_x(ld_x), .ld_y(ld_y), .in_x(init_x), .in_y(init_y), .reset_n(resetn), .x(w_x), .y(w_y), .output_colour(output_colour),
+	 .write(writeEn), .stateNum(stateNum), .init_y(init_y), .color(white));
    
     // Instansiate FSM control
     control c(.clk(CLOCK_50), .move_r(~KEY[0]), .move_l(~KEY[3]), .move_d(~KEY[1]), .move_u(~KEY[2]), .reset_n(resetn), .ld_x(ld_x), .ld_y(ld_y), .stateNum(stateNum), .reset_game(reset_game), .cc(ccc), .speed(tank));
-endmodule  
+endmodule   
+
+
+module reset_background(clock, drawEn, x_out, y_out, col_out);
+	input drawEn, clock;
+	output [6:0] x_out;
+	output [6:0] y_out;
+	output [2:0] col_out;
+	reg [6:0] x;
+	reg [6:0] y;
+	// mei count 6 ge x de bits, y jai 1
+	reg [11:0] counter;
+	assign col_out = 3'b000;
+	
+	always @(posedge clock)
+	begin
+	// 
+		if (!drawEn)
+			counter <= 12'b000000000000;
+		else if (drawEn)
+		begin
+		// ru guo dau di le
+			if (counter == 12'b111111111111)
+				counter <= 12'b000000000000;
+			else
+			// update the counter
+				counter <= counter + 1'b1;
+		end
+	end
+
+	assign x_out = x + counter[5:0];
+	assign y_out = y + counter[11:6];
+endmodule
 
 
 module control(clk, move_r, move_l, move_d, move_u, reset_n, ld_x, ld_y, stateNum, reset_game, cc, speed);
@@ -105,7 +161,7 @@ module control(clk, move_r, move_l, move_d, move_u, reset_n, ld_x, ld_y, stateNu
     wire result;
 	input [25:0] speed;
 
-	RateDividerForCar rd(clk, now, reset_n, speed);
+	RateDivider rd(clk, now, reset_n, speed);
 	 
     assign result = (now == cc) ? 1 : 0;
    
@@ -207,14 +263,14 @@ module control(clk, move_r, move_l, move_d, move_u, reset_n, ld_x, ld_y, stateNu
 
 endmodule
 
-module datapath(clk, ld_x, ld_y, in_x, in_y, reset_n, x, y, colour, stateNum, write, init_y, color);
+module datapath(clk, ld_x, ld_y, in_x, in_y, reset_n, x, y, output_colour, stateNum, write, init_y, color);
     input clk;
     input [6:0] in_x, in_y;
     input [6:0] init_y;
     input [2:0] color;
     input ld_x, ld_y;
     input reset_n;
-    output reg [2:0] colour;
+    output reg [2:0] output_colour;
     output reg write;
     output reg [6:0] y;
     output reg [6:0] x;
@@ -226,7 +282,7 @@ module datapath(clk, ld_x, ld_y, in_x, in_y, reset_n, x, y, colour, stateNum, wr
         begin
             x <= 6'b000000;
             y <= 6'b000000;
-            colour <= 3'b000;
+            output_colour <= 3'b000;
         end
         else
         begin
@@ -243,7 +299,7 @@ module datapath(clk, ld_x, ld_y, in_x, in_y, reset_n, x, y, colour, stateNum, wr
                
             else if(stateNum == 4'b0001)
                 begin
-                    colour <= 3'b000;
+                    output_colour <= 3'b000;
                     write <= 1'b1;
                 end
                
@@ -251,7 +307,7 @@ module datapath(clk, ld_x, ld_y, in_x, in_y, reset_n, x, y, colour, stateNum, wr
                 begin
                
                     x[6:0] <= x + 7'b000001;
-                    colour <= color;
+                    output_colour <= color;
                     write <= 1'b1;
                 end
                
@@ -259,7 +315,7 @@ module datapath(clk, ld_x, ld_y, in_x, in_y, reset_n, x, y, colour, stateNum, wr
                 begin
                
                     x[6:0] <= x - 7'b000001;
-                    colour <= color;
+                    output_colour <= color;
                     write <= 1'b1;
                 end
                
@@ -269,7 +325,7 @@ module datapath(clk, ld_x, ld_y, in_x, in_y, reset_n, x, y, colour, stateNum, wr
 							if (x != 7'b1110000)
 								begin
 								  y[6:0] <= y + 6'b000001;
-								  colour <= color;
+								  output_colour <= color;
 								  write <= 1'b1;
 								end
 							else
@@ -282,7 +338,7 @@ module datapath(clk, ld_x, ld_y, in_x, in_y, reset_n, x, y, colour, stateNum, wr
                 begin
                
                     y[6:0] <= y - 6'b000001;
-                    colour <= colour;
+                    output_colour <= output_colour;
                     write <= 1'b1;
                 end
 
@@ -308,4 +364,3 @@ module RateDivider (clock, q, Clear_b, speed);
             q <= q + 1'b1;
     end
 endmodule
-
