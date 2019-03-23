41bec9eda2d0082833868c21278e14e7bfa33b9a
Yu-TingWang <48304716+Yu-TingWang@users.noreply.github.com>
2019-03-23 18:52:50 -0400

    Add files via upload

--- display/display.v
+++ display/display.v
@@ -3,7 +3,6 @@ module display
 		clock,						//	On Board 50 MHz
 		// Your inputs and outputs here
         // KEY,
-		mode,
 		position,
 		address,
 		done,
@@ -11,28 +10,25 @@ module display
 		y,
 		colour
 	);
-	input [3:0] mode;
 	input [7:0] position;
 	input [7:0] address;
 	input clock;
 	output reg done;
 	output reg [3:0] x, y;
 	wire resetn;
-	wire [7:0] q;
-	
+	wire [5:0] q;
+	// access the data from storage to read
 	// Create the colour and writeEn wires that are inputs to the controller.
 	output reg [2:0] colour;
 	wire [3:0] row, col;
 	wire writeEn;
-	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
-	// for the VGA controller, in addition to any other functionality your design may require.
 	 
 	 // Tank Address is 8 bits binary; first 4 bits the column, last 4 bits the row.
-	 assign row = address[3:0];
-	 assign col = address[7:4];
+//	 assign row = address[3:0];
+//	 assign col = address[7:4];
 	 /*
 	 always @(posedge clock)
-	begin
+	beginq
 		begin
 		// assign tank1 a color
 			if (mode == 0001)
@@ -48,32 +44,40 @@ module display
 				colour <= 3'b110;
 		end
 	end
-	*/
+/* Wrapper module for storage of data. Access ram/registers through the following modes:
+* read ram: 0000 for display
+* edit ram: 1111
+*/
+		//always @(address)
+	//begin
+		// x times 16 (2^4)
+	assign start_x = (address[3:0] << 4);
+		// y times 12 (2^4-2^2)
+	assign start_y = (address[7:4] << 4) - (address[7:4] << 2);
+		//assign clear_b = 1'b1;
+	//end
 	
-	wire [3:0] start_x, start_y, gun_x, gun_y;
+	//assign col_out = col;
 	
-	assign start_x = (col << 4) + 3 - col;
-	assign start_y = (row << 4) + 4 - row;
-	assign gun_x = (col << 4) + 11 - col;
-	assign gun_y = (row << 4) + 7 - row;
+	wire [3:0] start_x, start_y;
+	// wire gun_x, gun_y;
+	// shift 4 to the left by timing 2^4,
+//	assign start_x = (col << 4) + 3 - col;
+//	assign start_y = (row << 4) + 4 - row;
+
 	
-	reg clear_b;
-	reg Enable;
-	
+	wire clear_b;
+	wire Enable;
+	/*
 	always @(address)
 	begin
 		clear_b <= 1'b0;
 		Enable <= 1'b1;
 	end
-// mode(according to storage.v):
-//* tank1 address: 0001
-//* tank1 : 0010
-//* tank1 projectile address: 0011
-//* tank1 projectile direction: 0100
-//* tank2 address: 0101
-//* tank2 direction: 0110
-//* tank2 projectile address: 0111
-//* tank2 projectile direction: 1000
+	*/
+	assign Enable = 1'b1;
+	assign clear_b = 1'b1;
+
 // position
 //8 bit ram (according to ram contract)
 //|_7_|___6__|__5__|_4__|_3_|__2_|____1____|____0____|
@@ -85,29 +89,15 @@ module display
 		if (position ==8'b01000000 || position == 8'b00100000)
 		begin
 		 // when it finish drawing
-			if (q == 6'd67)
+			if (q == 6'd63)
 				done <= 1'b1;
 			// draw the gun
-			else if (q == 7'd65)
-			begin
-				colour <= 3'b111;
-				x <= gun_x;
-				y <= gun_y;
-				done <= 1'b0;
-			end
-			else if (q == 7'd66)
-			begin
-				colour <= 3'b111;
-				x <= gun_x + 1;
-				y <= gun_y;
-				done <= 1'b0;
-				end
 			else
 			// draw the tank
 			begin
 				colour <= 3'b101;
-				x <= start_x + q[3:0];
-				y <= start_y + q[7:4];
+				x <= start_x + q[2:0];
+				y <= start_y + q[5:3];
 				done <= 1'b0;
 			end
 		end
@@ -120,8 +110,7 @@ module display
 			else
 			begin
 				colour <= 3'b110;
-				x <= start_x + q;
-				y <= start_y + 3;
+
 				done <= 1'b0;	
 			end
 		end
@@ -136,7 +125,7 @@ module display
 //				if (direction == 8'00000000)
 //				begin
 //					
-//				// if the projectile is going down
+//				// if the projecti		    .col_out(colour))le is going down
 //				else if (direction == 8'00000001)
 //				begin
 //				// if the projectile is going left
@@ -165,14 +154,14 @@ endmodule
 module counter(q, clear_b, clock, Enable);
 
 
-	output reg [7:0] q; // declare q
+	output reg [5:0] q; // declare q
 	input clear_b, clock, Enable;
 	
 	always @(posedge clock, negedge clear_b) // triggered every time clock rises
 		begin
 		if (clear_b == 1'b0) // when Clear_b is 0...
 			q <= 0; // set q to 0
-		else if (q == 8'b11111111) // ...otherwise if q is the maximum counter value
+		else if (q == 6'b111111) // ...otherwise if q is the maximum counter value
 			q <= 6'b0; // reset q to 0
 		else if (Enable == 1'b1) // ...otherwise update q (only when Enable is 1)
 			q <= q + 1'b1; // increment q
